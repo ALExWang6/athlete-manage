af8a505f50a8ce64d7c9997324baacdd8712ccb6
robuntu <robuntu@robuntu.ca>
2018-05-02 12:34:09 -0400

    task.java

--- app/models/Task.java
+++ app/models/Task.java
@@ -1,4 +1,26 @@
 package models;
 
-public class Task {
+import io.ebean.Finder;
+import io.ebean.Model;
+import play.data.validation.Constraints;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+public class Task extends Model {
+
+    public String user;
+
+    public String action;
+
+    public String object;
+
+
+//    public static Finder<>
+
+    public static String[] getLast(int x){
+
+        return Task.find.query().where().eq;
+
+    }
 }
