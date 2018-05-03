96ab1deab410eaf7bb2414923a65517d59b73822
MingzeJin <36276890+MingzeJin@users.noreply.github.com>
2018-05-03 13:59:03 -0400

    Update Task.java

--- app/models/Task.java
+++ app/models/Task.java
@@ -24,8 +24,8 @@ public class Task extends Model {
     }
     
     public static String getUser(String user) {
-        String firstName = user.substring(0, user.indexOf("."));
-        String lastName = user.substring(user.indexOf(".") + 1, user.indexOf("@") - 2);
+        String firstName = user.substring(0, 1).toUpperCase() + user.substring(1, user.indexOf("."));
+        String lastName = user.substring(user.indexOf(".") + 1, user.indexOf(".") + 2).toUpperCase() + user.substring(user.indexOf(".") + 2, user.indexOf("@") - 2);
         return firstName + " " + lastName;
     }
 }
